6536ca6f22b3b39f33871264242479c2bee3d68c
greysquareus <greysquareus@gmail.com>
2026-05-08 22:15:40 +0300

    init, project1 done

---
+++ Project_1/main.py
@@ -0,0 +1,8 @@
+user_input_one = input("What`s your name, buddy?\n")
+user_input_two = input("What`s your pet name?\n")
+
+
+test = print(f"Your user lengnth is:  {len(user_input_one)}")
+result = print(f"Okay {user_input_one} and {user_input_two} your answer, good\n")
+x = (user_input_one + user_input_two)
+final = print(f"Your rock team name is {x}")
